761f56faa684d9baf3357eaf64e6009da1818781
Shettu12345 <149598238+Shettu12345@users.noreply.github.com>
2023-11-01 15:40:42 +0100

    Add files via upload

--- shettu.py
+++ shettu.py
@@ -1,16 +1,12 @@
 Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> import re
->>> def find_and_replace(pattern, replace, string):
-...     match = re.search(pattern, string)
-...     if match:
-...         return re.sub(pattern, replace, string)
-...     else:
-...         return "No match found."
-... 
-...     
->>> string = "The quick brown fox jumps over the lazy cat."
->>> pattern = r"\b\w{5}\b"
->>> replace = "cat"
->>> print(find_and_replace(pattern, replace, string))
-The dog dog fox cat over the lazy cat.
+>>> def reverse_string(input_string):
+...     reversed_string = ""
+...     for char in input_string:
+...         if not char.isdigit():
+...             reversed_string = char + reversed_string
+...     return reversed_string
+...
+>>> input_string="shettu2023"
+>>> reverse_string(input_string)
+'UTTEHS'
